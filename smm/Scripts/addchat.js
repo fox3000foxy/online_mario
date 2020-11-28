bd935aa665ad41ab693acce1aec4fd0e825351bc
fox3000foxy <40730498+fox3000foxy@users.noreply.github.com>
2020-11-28 10:08:07 +0100

    Add files via upload

--- smm/Scripts/addchat.js
+++ smm/Scripts/addchat.js
@@ -1,28 +1,32 @@
   menu = document.createElement("ul")
   menu.setAttribute("src","addchat.js")
-  menu.setAttribute("style","position:absolute;right:0;top:-16px;width:20%;height:100%;background:white;border:1px solid black")
+  menu.setAttribute("style","position:absolute;right:0;top:-16px;width:17.55%;height:100%;background:white;border:1px solid black")
   document.getElementById("game").appendChild(menu);
   var page ='';
   var username = location.href.split("&username=")[1].split("&")[0]
-  var inputtext="<input type='text' style='width:75%;position:absolute;left:0;bottom:0' id='sender'>"
-  var inputsend="<button style='width:20%;position:absolute;left:80%;bottom:0;height:35px' id='baton' onclick='send()'>Send</button>"
   socket.on('chat', function(msg){
       msga = msg.split("&")
       if(msga[0]==salon)
       {
-      page += msga[1]
-      menu.innerHTML = "<br>"+page+inputtext+inputsend
+      page = msga[1]
+//       if(msga.length>=3){
+//           for(i=0;i<msga.length;i++)
+//           {
+//             page += "&"+msga[i]+"\n"
+//           }
+//     }
+      menu.innerHTML += "<br>"+page
       }
 })
-      menu.innerHTML = "<br>"+page+inputtext+inputsend
+      menu.innerHTML = "<br>"+page
 
   function send(){
     message = document.getElementById('sender').value
+    document.getElementById('sender').value = ''
+    document.getElementById('game').focus()
     if(message!="")
     {socket.emit("chat",salon+"&<b>"+username+":</b> "+message+"<br>")}
-    document.getElementById('sender').value = ''
 }
-
 
 socket.emit("chat",salon+"&<b>"+username+" connect</b><br>")
 
@@ -30,3 +34,11 @@ window.addEventListener("beforeunload", function (event) {
 socket.emit('chat',salon+'&<b>'+username+' disconnect</b><br>')
 //  event.preventDefault();
 });
+
+  input = document.createElement("div")
+  input.setAttribute("src","addchat.js")
+  input.setAttribute("style","position:absolute;right:0;bottom:0px;width:20%;height:100%")
+  var inputtext="<input type='text' style='width:75%;position:absolute;left:0;bottom:0' id='sender'>"
+  var inputsend="<button style='width:20%;position:absolute;left:80%;bottom:0;height:35px' id='baton' onclick='send()'>Send</button>"
+    document.getElementById("game").appendChild(input);
+  input.innerHTML = inputtext+inputsend
--- smm/Scripts/keys.js
+++ smm/Scripts/keys.js
@@ -18,6 +18,10 @@ var socket = io()
     
 setInterval("if(cam==-1){cam=counterplayer-1}if(cam==counterplayer){cam=0}",100)
 ok=1
+function pause()
+{
+    start=start==0?1:3
+}
 var keys = {
 	bind : function() {
 		$(document).on('keydown', function(event) {	
@@ -26,7 +30,6 @@ var keys = {
 			return keyspeach.handler(event, true,1,0);
 		});
 		$(document).on('keyup', function(event) {	
-    if(event.keyCode == 'P'.charCodeAt()){if(marioctrl==true){start=start==0?1:3;socket.emit("pause",start)}}
 			return keys.handler(event, false,1,0);
 			return keysluigi.handler(event, false,1,0);
 			return keyspeach.handler(event, false,1,0);
@@ -51,63 +54,32 @@ var keys = {
         
 	},
 	handler : function(event, status, withkeypad,elsebutton) {
+//         alert(event.keyCode)
 	if(withkeypad){
         if (ok==1 && document.activeElement==document.body){if(event.keyCode==13){document.getElementById('sender').focus();ok=0;setTimeout("ok=1",100);return}}
-        if (ok==1 && document.activeElement!=document.body){if(event.keyCode==13){document.getElementById("baton").click();document.body.focus();ok=0;setTimeout("ok=1",500)}return;}
-        switch(event.keyCode) {
-			case 57392://CTRL on MAC
-			case 17://CTRL
-            if (marioctrl==true){keys.accelerate = status;socket.emit("mario1",salon+"&"+status)}         
-				break;
-			case 40://DOWN ARROW
-			if (marioctrl==true){keys.down = status;socket.emit("mario2",salon+"&"+status)}
-                break;
-			case 39://RIGHT ARROW
-			if (marioctrl==true){keys.right = status;socket.emit("mario3",salon+"&"+status)}
-				break;
-			case 37://LEFT ARROW
-			if (marioctrl==true){keys.left = status;socket.emit("mario4",salon+"&"+status)}
-				break;
-			case 38://UP ARROW
-			if (marioctrl==true){keys.up = status;socket.emit("mario5",salon+"&"+status)}
-				break;
-			case 'A'.charCodeAt()://F
-			if (luigictrl==true){keysluigi.accelerate = status;socket.emit("luigi1",salon+"&"+status)}
- 				break;
-			case 'S'.charCodeAt()://
-			if (luigictrl==true){keysluigi.down = status;socket.emit("luigi2",salon+"&"+status)}
-  				break;
-			case 'D'.charCodeAt()://
-			if (luigictrl==true){keysluigi.right = status;socket.emit("luigi3",salon+"&"+status)}
-  				break;
-			case 'Q'.charCodeAt()://
-			if (luigictrl==true){keysluigi.left = status;socket.emit("luigi4",salon+"&"+status)}
- 			break;
-			case 'Z'.charCodeAt()://
-			if (luigictrl==true){keysluigi.up = status;socket.emit("luigi5",salon+"&"+status)}
-				break;
-            case 'N'.charCodeAt()://F
-			if (peachctrl==true){keyspeach.accelerate = status;socket.emit("peach1",salon+"&"+status)}
-				break;
-			case 'K'.charCodeAt()://
-			if (peachctrl==true){keyspeach.down = status;socket.emit("peach2",salon+"&"+status)}
-				break;
-			case 'L'.charCodeAt()://
-			if (peachctrl==true){keyspeach.right = status;socket.emit("peach3",salon+"&"+status)}
-				break;
-			case 'J'.charCodeAt()://
-			if (peachctrl==true){keyspeach.left = status;socket.emit("peach4",salon+"&"+status)}
- 				break;
-			case 'I'.charCodeAt()://
-			if (peachctrl==true){keyspeach.up = status;socket.emit("peach5",salon+"&"+status)}
- 				break;
-            case 'Y'.charCodeAt():/*CAMERA SWICTH*/ if (cam > 2){setTimeout('cam = 0',200);break;}else{setTimeout('cam += 0.5',200);break;}
-			case 'T'.charCodeAt():/*CAMERA SWICTH*/ if (cam < 0 ){setTimeout('cam = 2',200);break;}else{setTimeout('cam += -0.5',200);break;}
-            case 'U'.charCodeAt():break;
-			default:
-				return true;
+        if (ok==1 && document.activeElement!=document.body){if(event.keyCode==13){document.getElementById("baton").click();ok=0;setTimeout("ok=1",500)};return;}
+        if(event.keyCode==27 && document.activeElement!=document.body){document.body.focus()}
+            if(event.keyCode=='Y'.charCodeAt())/*CAMERA SWICTH*/ if (cam > 2){setTimeout('cam = 0',200);}else{setTimeout('cam += 0.5',200)}
+			if(event.keyCode=='T'.charCodeAt())/*CAMERA SWICTH*/ if (cam < 0 ){setTimeout('cam = 2',200);}else{setTimeout('cam += -0.5',200)}
+//            if(event.keyCode=='P'.charCodeAt())if(marioctrl==true){pause();socket.emit("pause",start)}
+            if(event.keyCode==57392)/*CAMERA SWICTH*/ if (marioctrl==true){keys.accelerate = status;socket.emit("mario1",salon+"&"+status)}    
+            if(event.keyCode==17)/*CAMERA SWICTH*/ if (marioctrl==true){keys.accelerate = status;socket.emit("mario1",salon+"&"+status)}    
+            if(event.keyCode==40)/*CAMERA SWICTH*/ if (marioctrl==true){keys.down = status;socket.emit("mario2",salon+"&"+status)}
+            if(event.keyCode==39)/*CAMERA SWICTH*/ if (marioctrl==true){keys.right = status;socket.emit("mario3",salon+"&"+status)}
+            if(event.keyCode==37)/*CAMERA SWICTH*/ if (marioctrl==true){keys.left = status;socket.emit("mario4",salon+"&"+status)}
+            if(event.keyCode==38)/*CAMERA SWICTH*/ if (marioctrl==true){keys.up = status;socket.emit("mario5",salon+"&"+status)}
+			if(event.keyCode==17)if (luigictrl==true){keysluigi.accelerate = status;socket.emit("luigi1",salon+"&"+status)}
+			if(event.keyCode==40)if (luigictrl==true){keysluigi.down = status;socket.emit("luigi2",salon+"&"+status)}
+            if(event.keyCode==39)if (luigictrl==true){keysluigi.right = status;socket.emit("luigi3",salon+"&"+status)}
+			if(event.keyCode==37)if (luigictrl==true){keysluigi.left = status;socket.emit("luigi4",salon+"&"+status)}
+			if(event.keyCode==38)if (luigictrl==true){keysluigi.up = status;socket.emit("luigi5",salon+"&"+status)}
+            if(event.keyCode==17)if (peachctrl==true){keyspeach.accelerate = status;socket.emit("peach1",salon+"&"+status)}
+			if(event.keyCode==40)if (peachctrl==true){keyspeach.down = status;socket.emit("peach2",salon+"&"+status)}
+			if(event.keyCode==39)if (peachctrl==true){keyspeach.right = status;socket.emit("peach3",salon+"&"+status)}
+			if(event.keyCode==37)if (peachctrl==true){keyspeach.left = status;socket.emit("peach4",salon+"&"+status)}
+			if(event.keyCode==38)if (peachctrl==true){keyspeach.up = status;socket.emit("peach5",salon+"&"+status)}
+			return true
 		}
-    }
     else{
 				switch(elsebutton) {
                     case "fireball":
